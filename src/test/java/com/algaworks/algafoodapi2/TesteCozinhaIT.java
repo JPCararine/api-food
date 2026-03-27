aea29f89a04f2286849608eae0051666dffa0bea
João <joaopedrocararinerangel@gmail.com>
2026-03-27 12:42:04 -0300

    Alguns testes adicionados e metodo pra limpar e popular o banco de dados de teste

--- src/test/java/com/algaworks/algafoodapi2/TesteCozinhaIT.java
+++ src/test/java/com/algaworks/algafoodapi2/TesteCozinhaIT.java
@@ -1,27 +1,48 @@
 package com.algaworks.algafoodapi2;
 
+import static io.restassured.RestAssured.get;
 import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.equalTo;
 
+import com.algaworks.algafoodapi2.domain.model.Cozinha;
+import com.algaworks.algafoodapi2.repository.CozinhaRepository;
 import io.restassured.RestAssured;
 import io.restassured.http.ContentType;
 import org.hamcrest.Matchers;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.server.LocalServerPort;
 import org.springframework.http.HttpStatus;
+import org.springframework.test.context.TestPropertySource;
+import com.algaworks.algafoodapi2.util.DatabaseCleaner;
+
+import java.net.URL;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@TestPropertySource("/application-teste.properties")
 public class TesteCozinhaIT {
 
     @LocalServerPort
     private int port;
+
+    @Autowired
+    private DatabaseCleaner databaseCleaner;
+    @Autowired
+    private CozinhaRepository cozinhaRepository;
+
+
 
     @BeforeEach
     public void setUp() {
         RestAssured.enableLoggingOfRequestAndResponseIfValidationFails();
         RestAssured.basePath = "/cozinhas";
         RestAssured.port = port;
+
+        databaseCleaner.clearTables();
+        prepararDados();
+
     }
 
 
@@ -41,7 +62,7 @@ public class TesteCozinhaIT {
                 .when()
                 .get()
                 .then()
-                .body("nome", Matchers.hasSize(4));
+                .body("nome", Matchers.hasSize(2));
     }
 
     @Test
@@ -56,5 +77,38 @@ public class TesteCozinhaIT {
                 .statusCode(HttpStatus.CREATED.value());
     }
 
+    @Test
+    public void deveRetornarRespostaEStatusQuandoGetCozinhasExistente() {
+        given()
+                .pathParam("id", 2)
+                .accept(ContentType.JSON)
+                .when()
+                .get("/{id}")
+                .then()
+                .statusCode(HttpStatus.OK.value())
+                .body("nome", equalTo("Japonesa"));
+
+    }
+    @Test
+    public void deveRetornarErro404RespostaEStatusQuandoGetCozinhasInexistente() {
+        given()
+                .pathParam("id", 4)
+                .accept(ContentType.JSON)
+                .when()
+                .get("/{id}")
+                .then()
+                .statusCode(HttpStatus.NOT_FOUND.value());
+
+
+    }
+
+    private void prepararDados() {
+        Cozinha cozinha1 = new Cozinha();
+        cozinha1.setNome("Tailandesa");
+        cozinhaRepository.save(cozinha1);
+        Cozinha cozinha2 = new Cozinha();
+        cozinha2.setNome("Japonesa");
+        cozinhaRepository.save(cozinha2);
+    }
 }
 
